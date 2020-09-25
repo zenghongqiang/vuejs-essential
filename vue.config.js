458de1ac873d25e787475d70c69b5126082695d2
hqtsang <zenghongqiang44@163.com>
2020-09-26 02:37:56 +0800

    add github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/vuejs-essential/dist/'
-      : './'
+    devServer: {
+      port: 8082,   // 端口号
+    },
+    publicPath: '/vuejs-essential/dist/',  
   }
